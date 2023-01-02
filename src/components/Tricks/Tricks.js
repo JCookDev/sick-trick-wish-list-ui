b30cb1441279a60d20fca5b6c270aa0fcb6f3fd1
Jeff Cook <cookturingedu@gmail.com>
2023-01-02 16:51:34 -0500

    Iterate over the mock data and pass it to the cards component

--- src/components/Tricks/Tricks.js
+++ src/components/Tricks/Tricks.js
@@ -3,9 +3,21 @@ import Card from '../Card/Card'
 import '../Tricks/Tricks.css';
 
 const Tricks = ({ tricks }) => {
+    const trickCards = tricks.map(trick => {
+        return(
+            <Card
+             stance= {trick.stance}
+             name= {trick.name}
+             obstacle= {trick.obstacle}
+             tutorial= {trick.tutorial}
+             id= {trick.id}
+             key= {trick.id}
+            />
+        )
+    })
     return (
         <div className= 'tricks-container'>
-            <Card  />
+            {trickCards}
         </div>
     )
 }
