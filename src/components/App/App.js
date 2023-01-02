6dbb3a3c268aa382d67282d837b1a34ad909e1fa
Jeff Cook <cookturingedu@gmail.com>
2023-01-02 15:45:19 -0500

    Initial Commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,14 @@ import { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      
+    }
+  }
+
+
   render() {
     return (
       <div className="App">
